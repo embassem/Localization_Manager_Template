d291de4937d24fb311cc8db5fba57b3b612496ea
Bassem Abbas <bassem.abbas@ibtikar.net.sa>
2020-01-14 11:47:15 +0200

    format file

--- Localization Manager.xctemplate/Localization+Extension.swift
+++ Localization Manager.xctemplate/Localization+Extension.swift
@@ -10,37 +10,36 @@
 import UIKit
 
 extension Bundle {
-
-	@objc
-	func appLocalizedString(forKey key: String, value: String?, table tableName: String?) -> String {
-		// check if its the main bundle then if the bundle of the current language is available
-		// then try without locale
-		// if not go back to base
-
-		let translate =  { (tableName: String?) -> String in
-			let currentLanguage = LocalizationManager.currentLanguage().languageCode // with locale
-			let languageWithoutLocale = LocalizationManager.currentLanguage().languageCode// without locale
-			var bundle = Bundle();
-			// normal case where the lang with locale working
-			if let _path = Bundle.main.path(forResource: currentLanguage, ofType: "lproj") {
-				bundle = Bundle(path: _path)!
-			} // en case when its working wihout locale
-			else if let _path = Bundle.main.path(forResource: languageWithoutLocale, ofType: "lproj") {
-				bundle = Bundle(path: _path)!
-			} // current locale not exist , so we fallback
-			else if let _path = Bundle.main.path(forResource: "Base", ofType: "lproj") {
-				bundle = Bundle(path: _path)!
-			}
-			return (bundle.appLocalizedString(forKey: key, value: value, table: tableName))
-		}
-		// normal case
-		if self == Bundle.main {
-			return translate(tableName)
-		} // let the bundle handle the locale
-		else {
-			return (self.appLocalizedString(forKey: key, value: value, table: tableName))
-		}
-	}
+    
+    @objc
+    func appLocalizedString(forKey key: String, value: String?, table tableName: String?) -> String {
+        // check if its the main bundle then if the bundle of the current language is available
+        // then try without locale
+        // if not go back to base
+        let translate =  { (tableName: String?) -> String in
+            let currentLanguage = LocalizationManager.currentLanguage().languageCode // with locale
+            let languageWithoutLocale = LocalizationManager.currentLanguage().languageCode// without locale
+            var bundle = Bundle();
+            // normal case where the lang with locale working
+            if let _path = Bundle.main.path(forResource: currentLanguage, ofType: "lproj") {
+                bundle = Bundle(path: _path)!
+            } // en case when its working wihout locale
+            else if let _path = Bundle.main.path(forResource: languageWithoutLocale, ofType: "lproj") {
+                bundle = Bundle(path: _path)!
+            } // current locale not exist , so we fallback
+            else if let _path = Bundle.main.path(forResource: "Base", ofType: "lproj") {
+                bundle = Bundle(path: _path)!
+            }
+            return (bundle.appLocalizedString(forKey: key, value: value, table: tableName))
+        }
+        // normal case
+        if self == Bundle.main {
+            return translate(tableName)
+        } // let the bundle handle the locale
+        else {
+            return (self.appLocalizedString(forKey: key, value: value, table: tableName))
+        }
+    }
 }
 
 extension UIApplication {
